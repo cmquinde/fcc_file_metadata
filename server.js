8c3730cad91f33cbc7c66c16d4c729c70d89fb84
CrisMer Quinde <cmquinde@espol.edu.ec>
2022-03-02 19:22:54 +0000

    Enviar un formulario que incluya una carga de archivo

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var cors = require('cors');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 require('dotenv').config()
 
 var app = express();
@@ -10,8 +13,12 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
+ 
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  console.log(res)
+  res.json({ name: req.file.originalname, size: req.file.size, type: req.file.mimetype });
+});
 
 
 const port = process.env.PORT || 3000;
